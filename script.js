6c1064223332918e90b98e49415d74777891af3e
itpRakib <rakibulislam.cse.41230301525@gmail.com>
2024-11-16 15:58:51 +0600

    n

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-const messageInput= document.querySelector('.message-input');
+const chatBody = document.querySelector('.chat-body');
+const messageInput = document.querySelector('.message-input');
+const sendMessageButton = document.querySelector('#send-message');
 
-messageInput.addEventListener('keydown',(e)=>{
+const userData = {
+    message: null
+}
+
+const createMessageElement =(content, classes) =>{
+    const div = document.createElement('div');
+    div.classList.add('message', classes);
+    div.innerHTML = content;
+    return div;
+}
+
+const handleOutgoingMessage = (e) => {
+    e.preventDefault();
+    userData.message = messageInput.value.trim();
+    messageInput.value = '' ;
+
+    const messageContent = ` <div class="message-text"></div>`;
+    const outgoingMessageDiv = createMessageElement(messageContent, 'user-message');
+    outgoingMessageDiv.querySelector('.message-text').textContent =  userData.message;
+    chatBody.appendChild(outgoingMessageDiv);
+}
+
+messageInput.addEventListener('keydown', (e) => {
     const userMessage = e.target.value.trim();
-    if(e.key === 'Enter' && userMessage ){
-        console.log(userMessage);
-        
+    if (e.key === 'Enter' && userMessage) {
+        handleOutgoingMessage(e);
+
     }
 })
+
+sendMessageButton.addEventListener('click' , (e) => handleOutgoingMessage(e))
