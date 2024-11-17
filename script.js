8cee43df8edc20ba60dc8445020fa44ede9bd8b8
itpRakib <rakibulislam.cse.41230301525@gmail.com>
2024-11-18 00:17:37 +0600

    n

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const createMessageElement = (content, ...classes) => {
 
 const generateBotResponse = async (incomingMessageDiv) => {
     const messageElement = incomingMessageDiv.querySelector
-    ('.message-text');
+        ('.message-text');
 
     const requestOptions = {
         method: 'POST',
@@ -32,30 +32,38 @@ const generateBotResponse = async (incomingMessageDiv) => {
     try {
         const response = await fetch(API_URL, requestOptions);
         const data = await response.json();
-        if(!response.ok) throw new Error(data.error.message);
+        if (!response.ok) throw new Error(data.error.message);
 
-        const apiResponseText = data.candidates[0].content.parts[0].text.trim();
+        const apiResponseText = data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, '$1').trim();
         messageElement.innerText = apiResponseText;
     } catch (error) {
         console.log(error);
-        
+        messageElement.innerText = error.message;
+        messageElement.style.color = '#ff0000';
     }
-
+    finally {
+        incomingMessageDiv.classList.remove('thinking');
+        chatBody.scrollTo({top: chatBody.scrollHeight, behavior: 'smooth'});
+    }
 }
+
+
 
 const handleOutgoingMessage = (e) => {
     e.preventDefault();
     userData.message = messageInput.value.trim();
     messageInput.value = '';
 
+
     const messageContent = ` <div class="message-text"></div>`;
 
     const outgoingMessageDiv = createMessageElement(messageContent, 'user-message');
     outgoingMessageDiv.querySelector('.message-text').textContent = userData.message;
     chatBody.appendChild(outgoingMessageDiv);
+    chatBody.scrollTo({top: chatBody.scrollHeight, behavior: 'smooth'});
 
     setTimeout(() => {
-        const messageContent = `  <svg class="bot-avater" xmlns="http://www.w3.org/2000/svg" width="50" height="50"
+        const messageContent = `<svg class="bot-avater" xmlns="http://www.w3.org/2000/svg" width="50" height="50"
                     viewBox="0 0 1024 1024">
                     <path
                         d="M738.3 287.6H285.7c-59 0-106.8 47.8-106.8 106.8v303.1c0 59 47.8 106.8 106.8 106.8h81.5v111.1c0 .7.8 1.1 1.4.7l166.9-110.6 41.8-.8h117.4l43.6-.4c59 0 106.8-47.8 106.8-106.8V394.5c0-59-47.8-106.9-106.8-106.9zM351.7 448.2c0-29.5 23.9-53.5 53.5-53.5s53.5 23.9 53.5 53.5-23.9 53.5-53.5 53.5-53.5-23.9-53.5-53.5zm157.9 267.1c-67.8 0-123.8-47.5-132.3-109h264.6c-8.6 61.5-64.5 109-132.3 109zm110-213.7c-29.5 0-53.5-23.9-53.5-53.5s23.9-53.5 53.5-53.5 53.5 23.9 53.5 53.5-23.9 53.5-53.5 53.5zM867.2 644.5V453.1h26.5c19.4 0 35.1 15.7 35.1 35.1v121.1c0 19.4-15.7 35.1-35.1 35.1h-26.5zM95.2 609.4V488.2c0-19.4 15.7-35.1 35.1-35.1h26.5v191.3h-26.5c-19.4 0-35.1-15.7-35.1-35.1zM561.5 149.6c0 23.4-15.6 43.3-36.9 49.7v44.9h-30v-44.9c-21.4-6.5-36.9-26.3-36.9-49.7 0-28.6 23.3-51.9 51.9-51.9s51.9 23.3 51.9 51.9z">
@@ -71,6 +79,7 @@ const handleOutgoingMessage = (e) => {
 
         const incomingMessageDiv = createMessageElement(messageContent, 'bot-message', 'thinking');
         chatBody.appendChild(incomingMessageDiv);
+        chatBody.scrollTo({top: chatBody.scrollHeight, behavior: 'smooth'});
         generateBotResponse(incomingMessageDiv);
     }, 600);
 }
